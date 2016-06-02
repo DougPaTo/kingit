b5f7a907ce1e6b390b95720cf5b2578359e9fe2d
helladarion <helladarion@gmail.com>
2016-06-02 01:17:27 -0700

    fixed minor errors for testing purposes

--- VPN/vpn.sh
+++ VPN/vpn.sh
@@ -516,7 +516,7 @@ echo "route add -net $V_SNet netmask $V_SMask gw $(echo $V_ConIP | sed 's/.$/1/'
 
 echo "Including VPN on the startup"
 
-sed -i "s|exit 0|/root/$TunFile\n&|" /etc/rc.local
+sed -i "s|^exit 0|cd /root/confs/client/\nsh startsrv_$V_Port.sh\n&|" /etc/rc.local
 }
 
 function adjustPortForward(){
